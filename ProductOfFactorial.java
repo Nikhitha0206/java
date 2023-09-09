86b4773bd4a97fb05fe71b1c750aad230bbdfc90
Nikhitha0206 <143926714+Nikhitha0206@users.noreply.github.com>
2023-09-09 09:49:52 +0530

    Add files via upload

---
+++ ProductOfFactorial.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class ProductOfFactorial{
+	public static void main(String[] args){
+		Scanner sc=new scanner(system.in);	
+		System.out.println("Enter n:");
+		int fact=1;
+		for(i=2;i<=n;i++)
+		{
+			fact=fact*i;
+		}
+		Syatem.out.println("product of " +n+ "is" +fact);
+	}
+}
+
